6fef99a512e4241800516299202a688032883352
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-02 01:58:08 +0300

    set a condition in the label tag of displaying red or black

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -6,6 +6,7 @@ import './CourseInput.css';
 const CourseInput = (props) => {
 
     const [enteredValue, setEnteredValue] = useState('');
+    const [isValid, setIsValid] = useState(true);
 
     const goalInputChangeHandler = event => {
         setEnteredValue(event.target.value);
@@ -22,7 +23,7 @@ const CourseInput = (props) => {
     return (
         <form onSubmit={formSubmitHandler}>
             <div className='form-control'>
-                <label>Course Goal</label>
+                <label style={{ color: !isValid ? 'red' : 'black'}}>Course Goal</label>
                 <input type='text' onChange={goalInputChangeHandler} />
             </div>
             <Button type='submit'>Add Goal</Button>
