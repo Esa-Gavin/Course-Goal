147d8c27da00b8ec09ffc4ebab5d2c2b7f6062ba
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-02 02:51:12 +0300

    change the label tag color back to red

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -25,7 +25,7 @@ const CourseInput = (props) => {
     return (
         <form onSubmit={formSubmitHandler}>
             <div className='form-control'>
-                <label style={{ color: !isValid ? 'salmon' : 'black'}}>Course Goal</label>
+                <label style={{ color: !isValid ? 'red' : 'black'}}>Course Goal</label>
                 <input style={{ borderColor: !isValid ? 'salmon' : 'black', background: !isValid ? 'salmon' : 'transparent'}} type='text' onChange={goalInputChangeHandler} />
             </div>
             <Button type='submit'>Add Goal</Button>
