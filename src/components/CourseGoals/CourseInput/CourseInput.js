b1b5a527b34d089f3dc3c30f7a7ac2194e241190
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-02 03:22:28 +0300

    add an if statement

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { flushSync } from 'react-dom';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
@@ -10,6 +9,9 @@ const CourseInput = (props) => {
     const [isValid, setIsValid] = useState(true);
 
     const goalInputChangeHandler = event => {
+        if (event.target.value.trim().length === 0) {
+            setIsValid(true);
+        }
         setEnteredValue(event.target.value);
     }
 
