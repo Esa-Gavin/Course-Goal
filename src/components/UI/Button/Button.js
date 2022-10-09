21f8f5dad29b9ea8bd75d6df5e74d2550035b68a
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-10 02:21:58 +0300

    import styled components and also create a component named Button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import './Button.css';
 
-const Button = (props) => {
-    return (
-        <button type={props.type} className='button' onClick={props.onClick}>
-            {props.children}
-        </button>
-    );
-};
+// the '.button' indicates that it is a method, //
+// so any html tag can be used instead, //
+// and inside the backticks is where we shall have our content //
+const Button = styled.button``;
+
+//const Button = (props) => {
+//  return (
+//        <button type={props.type} className='button' onClick={props.onClick}>
+//            {props.children}
+//        </button>
+//    );
+//};
 
 export default Button;
