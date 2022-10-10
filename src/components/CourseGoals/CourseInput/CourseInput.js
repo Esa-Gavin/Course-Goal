578b31f8fb5a0ac8f9d72d9528654b21dc2cb765
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-11 02:20:54 +0300

    add a divcontrol style component for the div that was inside the form tag

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,46 @@
 import React, { useState } from 'react';
 
+import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+const DivControl = styled.div`
+    
+    margin: 0.5rem 0;
+    
+
+    & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    }
+
+    & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+    }
+
+    & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+    }
+
+    &.invalid input {
+    border-color: red;
+    background: #ffd7d7;
+    }
+
+    &.invalid label {
+    color: red;
+    }
+
+
+`;
 
 const CourseInput = (props) => {
 
@@ -26,10 +65,10 @@ const CourseInput = (props) => {
     // if the input is invalid the color of the input will be red otherwise it would be black//
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+            <DivControl className={`form-control ${!isValid ? 'invalid' : ''}`}>
                 <label>Course Goal</label>
                 <input type='text' onChange={goalInputChangeHandler} />
-            </div>
+            </DivControl>
             <Button type='submit'>Add Goal</Button>
         </form>
     )
