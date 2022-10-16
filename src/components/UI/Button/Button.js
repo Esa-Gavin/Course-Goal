e7642ae4ad59c670af2ee88e384bffb29a37a2eb
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-16 21:50:55 +0300

    learn how to use media queries in styled components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -6,6 +6,7 @@ import './Button.css';
 // so any html tag can be used instead, //
 // and inside the backticks is where we shall have our content //
 const Button = styled.button`
+    width: 100%;
     font: inherit;
     padding: 0.5rem 1.5rem;
     border: 1px solid #8b005d;
@@ -13,6 +14,10 @@ const Button = styled.button`
     background: #8b005d;
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
     cursor: pointer;
+
+    @media (min-width: 768px) {
+        width: auto;
+    }
 
 
     &:focus {
