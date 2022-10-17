5f5ea618ca22515de256962f190b7192959f4ae6
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-17 08:08:09 +0300

    learn how to use css modules dynamically and how to integrate media queries

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,39 +1,39 @@
 import React, { useState } from 'react';
 
-import styled from 'styled-components';
+//import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import classes from './CourseInput.module.css';
 
-const DivControl = styled.div`
+// const DivControl = styled.div`
     
-    margin: 0.5rem 0;
+//     margin: 0.5rem 0;
     
 
-    & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => (props.invalid ? 'red' : 'black')};
-    }
+//     & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => (props.invalid ? 'red' : 'black')};
+//     }
 
-    & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
-    background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-    }
+//     & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+//     background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//     }
 
-    & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-    }
+//     & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//     }
 
 
-`;
+// `;
 
 const CourseInput = (props) => {
 
@@ -61,10 +61,10 @@ const CourseInput = (props) => {
     // if the input is invalid the color of the input will be red otherwise it would be black//
     return (
         <form onSubmit={formSubmitHandler}>
-            <DivControl invalid={!isValid}>
+            <div className={`${classes['form-control']} ${!isValid && classes.invalid}`}>
                 <label>Course Goal</label>
                 <input type='text' onChange={goalInputChangeHandler} />
-            </DivControl>
+            </div>
             <Button type='submit'>Add Goal</Button>
         </form>
     )
